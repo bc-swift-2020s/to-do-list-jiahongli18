bcdc6b0f37f317d8a839f756818aa2763e881fde
Jiahong Li <libya@bc.edu>
2020-02-07 13:38:20 -0500

    finished up to 5.6

---
+++ ToDoList/ToDoDetailTableViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ToDoDetailTableViewController.swift
+//  ToDoList
+//
+//  Created by Jia H Li on 2/6/20.
+//  Copyright © 2020 Jia H Li. All rights reserved.
+//
+
+import UIKit
+
+class ToDoDetailTableViewController: UITableViewController {
+
+    @IBOutlet weak var saveBarButton: UIBarButtonItem!
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var noteView: UITextView!
+    
+    var toDoItem: ToDoItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if toDoItem == nil{
+            toDoItem = ToDoItem(name: "", date: Date(), notes: "")
+        }
+        
+        nameField.text = toDoItem.name
+        datePicker.date = toDoItem.date
+        noteView.text = toDoItem.notes
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
+        toDoItem = ToDoItem(name: nameField.text!, date: datePicker.date, notes: noteView.text)
+    }
+
+    @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
+        let isPresentingInAddMode = presentingViewController is UINavigationController
+        if isPresentingInAddMode {
+            dismiss(animated: true, completion: nil)
+        } else {
+            navigationController?.popViewController(animated: true)
+        }
+    }
+    
+}
---
+++ ToDoList/ToDoItem.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoItem.swift
+//  ToDoList
+//
+//  Created by Jia H Li on 2/7/20.
+//  Copyright © 2020 Jia H Li. All rights reserved.
+//
+
+import Foundation
+
+struct ToDoItem {
+    var name: String
+    var date: Date
+    var notes: String
+}
